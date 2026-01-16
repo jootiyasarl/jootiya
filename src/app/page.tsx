73b13b7338fd0a7a55e41d28317842141e84aa76
Bartouchy <bartouchy@example.com>
2026-01-16 14:29:36 +0100

    Initial postin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -153,9 +153,12 @@ export default function Home() {
                 <button className="inline-flex items-center justify-center rounded-full bg-zinc-900 px-4 py-2 text-sm font-medium text-zinc-50">
                   Start browsing near me
                 </button>
-                <button className="inline-flex items-center justify-center rounded-full border border-zinc-200 bg-white px-4 py-2 text-sm font-medium text-zinc-800">
+                <Link
+                  href="/dashboard/ads/create"
+                  className="inline-flex items-center justify-center rounded-full border border-zinc-200 bg-white px-4 py-2 text-sm font-medium text-zinc-800"
+                >
                   Post your first ad
-                </button>
+                </Link>
               </div>
             </div>
             <div className="hidden flex-col justify-between gap-4 rounded-2xl border border-dashed border-zinc-200 bg-zinc-50 p-4 text-sm text-zinc-500 sm:flex">
