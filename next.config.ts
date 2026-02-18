848a7ced952280ad985e1dd16d43a63923f61673
Bartouchy <bartouchy@example.com>
2026-02-18 03:21:34 +0000

    accessibility: allow user zooming and add security headers

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,37 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    const securityHeaders = [
+      {
+        key: "X-DNS-Prefetch-Control",
+        value: "on",
+      },
+      {
+        key: "Strict-Transport-Security",
+        value: "max-age=63072000; includeSubDomains; preload",
+      },
+      {
+        key: "X-Frame-Options",
+        value: "SAMEORIGIN",
+      },
+      {
+        key: "X-Content-Type-Options",
+        value: "nosniff",
+      },
+      {
+        key: "Referrer-Policy",
+        value: "origin-when-cross-origin",
+      },
+    ];
+
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
