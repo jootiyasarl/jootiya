b23568a27cd6f03ad4b07fba73e657af8ae6a1a1
Bartouchy <bartouchy@example.com>
2026-02-26 02:11:32 +0000

    analytic txt code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ import { ScrollToTop } from "@/components/layout/ScrollToTop";
 import { Suspense } from "react";
 import { ThemeProvider } from "@/components/theme-provider";
 import { PageViewTracker } from "@/components/analytics/PageViewTracker";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const tajawal = Tajawal({
   variable: "--font-tajawal",
@@ -164,6 +165,7 @@ export default function RootLayout({
           <PushPermissionPrompt />
           <Toaster position="top-center" richColors />
         </ThemeProvider>
+        <GoogleAnalytics gaId="G-2BS0D7DTDJ" />
 
         {/* 5. Google Adsense & GDPR Scripts (Lazy Loaded at Bottom) */}
         <Script
