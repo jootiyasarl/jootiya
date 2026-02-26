b8df62bd17f7793ac06181456475d77671668d34
Bartouchy <bartouchy@example.com>
2026-02-26 02:24:42 +0000

    dd oogle ag anager () to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -151,12 +151,31 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GT-MKRC853R');
+          `}
+        </Script>
       </head>
 
       <body
         className={`${tajawal.variable} ${inter.variable} font-sans antialiased overflow-x-hidden bg-white dark:bg-zinc-950`}
         style={{ backgroundColor: '#ffffff' }}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GT-MKRC853R"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         {/* المحتوى المخفي لرفع سعر النقرة (CPC Injection) */}
         <h1 className="sr-only">Jootiya: Analyse du Marché Marocain, Infrastructure Logistique et Opportunités d'Investissement</h1>
 
