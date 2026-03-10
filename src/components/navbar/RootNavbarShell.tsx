dae36f2165832f831eb88538f19b750dfc14f7d9
Bartouchy <bartouchy@example.com>
2026-03-10 03:27:12 +0000

    fix: remove PageTransition to eliminate mobile application error

--- src/components/navbar/RootNavbarShell.tsx
+++ src/components/navbar/RootNavbarShell.tsx
@@ -3,7 +3,6 @@
 import { ReactNode, Suspense, useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { PageTransition } from "@/components/layout/PageTransition";
 import dynamic from "next/dynamic";
 
 const MobileBottomNav = dynamic(() => import("./MobileBottomNav").then(mod => mod.MobileBottomNav), {
@@ -75,30 +74,28 @@ export function RootNavbarShell({ children, navbar, footer }: RootNavbarShellPro
         !isSpecialPath && "pt-[56px] md:pt-[64px]"
       )}>
         <div className={cn(!isSpecialPath && "main-content-wrapper")}>
-          <PageTransition>
-            {!isSpecialPath ? (
-              <div className="max-w-[1440px] mx-auto px-4 md:px-8 relative">
-                <div className="flex flex-col lg:flex-row gap-6 lg:gap-8 items-start relative">
-                  {/* Left Global Sidebar Ad */}
-                  <aside className="hidden xl:block w-40 sticky top-24 shrink-0">
-                    <SidebarAd />
-                  </aside>
+          {!isSpecialPath ? (
+            <div className="max-w-[1440px] mx-auto px-4 md:px-8 relative">
+              <div className="flex flex-col lg:flex-row gap-6 lg:gap-8 items-start relative">
+                {/* Left Global Sidebar Ad */}
+                <aside className="hidden xl:block w-40 sticky top-24 shrink-0">
+                  <SidebarAd />
+                </aside>
 
-                  {/* Main Page Content */}
-                  <div className="flex-1 min-w-0 w-full">
-                    {children}
-                  </div>
-
-                  {/* Right Global Sidebar Ad */}
-                  <aside className="hidden xl:block w-40 sticky top-24 shrink-0">
-                    <SidebarAd />
-                  </aside>
+                {/* Main Page Content */}
+                <div className="flex-1 min-w-0 w-full">
+                  {children}
                 </div>
+
+                {/* Right Global Sidebar Ad */}
+                <aside className="hidden xl:block w-40 sticky top-24 shrink-0">
+                  <SidebarAd />
+                </aside>
               </div>
-            ) : (
-              children
-            )}
-          </PageTransition>
+            </div>
+          ) : (
+            children
+          )}
         </div>
       </main>
       {!isSpecialPath && (
