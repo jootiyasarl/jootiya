7c5f058a7c369df5be291d29f7d8a86d21129d4a
Bartouchy <bartouchy@example.com>
2026-06-20 15:12:59 +0100

    Fix: remove spurious 'Aucun resultat trouve' block on search page

--- src/components/marketplace/search/MarketplaceSearchLayout.tsx
+++ src/components/marketplace/search/MarketplaceSearchLayout.tsx
@@ -3,14 +3,11 @@
 import type { MarketplaceSearchLayoutProps } from "@/types/components/marketplace";
 import { MarketplaceFilterSidebar } from "../filters/MarketplaceFilterSidebar";
 import { SortDropdown } from "../filters/SortDropdown";
-import { ListingGrid } from "../listing/ListingGrid";
 import { MobileFilterTrigger } from "../filters/MobileFilterTrigger";
 
 export function MarketplaceSearchLayout({
-  searchBarProps,
   filterSidebarProps,
   sortDropdownProps,
-  listingGridProps,
 }: MarketplaceSearchLayoutProps) {
   return (
     <div className="mx-auto flex w-full max-w-7xl flex-col gap-8 px-4 pb-12">
@@ -23,9 +20,6 @@ export function MarketplaceSearchLayout({
 
       <div className="flex gap-6">
         <MarketplaceFilterSidebar {...filterSidebarProps} />
-        <div className="flex-1">
-          <ListingGrid {...listingGridProps} />
-        </div>
       </div>
     </div>
   );
